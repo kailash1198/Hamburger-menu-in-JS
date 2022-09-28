a1b4f09a4f4541e78b23b4ac963d7c65afb42ce9
Dominika666 <zajacowna666@gmail.com>
2022-09-28 10:27:16 +0200

    Add new files

---
+++ js.js
@@ -0,0 +1,14 @@
+const burgerBtn = document.querySelector('.burger')
+const barsIco = document.querySelector('.fa-bars')
+const xIco = document.querySelector('.fa-x')
+const nav = document.querySelector('nav ul')
+
+
+const handleNav = () => {
+nav.classList.toggle('active')
+burgerBtn.classList.toggle('active')
+barsIco.classList.toggle('hide')
+xIco.classList.toggle('hide')
+}
+
+burgerBtn.addEventListener('click', handleNav)
